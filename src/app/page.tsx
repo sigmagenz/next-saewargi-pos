0eaf5d4739659eb7da610410dc7bfac7c302d540
zeinirfansyah <12.zeinirfansyah@gmail.com>
2025-07-13 21:05:24 +0700

    feat [theme]: implement theme mode toggle and provider for theme management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import { ThemeModeToggle } from "@/components/common/theme-mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
         <h1 className="font-semibold text-2xl">Halo Welcome to My App</h1>
-        <Button variant="default" size="lg">
+        <Button variant="default" size="lg" className="dark:bg-gray-200">
           Click me!
         </Button>
+        <ThemeModeToggle />
       </div>
     </>
   );
